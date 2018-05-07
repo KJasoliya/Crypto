681becfbcd0a0dcc739097686d30e177b0e7de63
Kinnari Jasoliya <kinnarijasoliya2606@gmail.com>
2018-05-07 18:14:05 -0400

    Context change

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
         <div class="inner">
             <h2>Crypto site</h2>
             <h4>
-                This website contains source code & demo of various Ciphers & Algorithms<br>
+                This website contains demo of various Ciphers & Algorithms<br>
                 Checkout The Demo Section<b><a href="ciphers.php"> here</a></b><br>
-                Free Sign Up to record your history and download Source Codes in php
+                Free Sign Up to record your history
             </h4>
             <ul class="actions">
                 <li><a href="signup.php" class="button big special">Sign Up</a></li>
